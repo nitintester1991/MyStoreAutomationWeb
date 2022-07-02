36587e33079536631c99de8da4b7bc1bb633f7e0
nitintester1991 <nitin.tester1991@gmail.com>
2022-07-02 16:31:13 +0530

    First commit for MyStore Automation

---
+++ src/launchWrbSite.java
@@ -0,0 +1,11 @@
+
+public class launchWrbSite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("Webdriver.chrome.driver", "C:\\Python38\\Scripts\\chromedriver.exe");
+//		ChromeDriver driver= new ChromeDriver();
+//		driver.get("http://automationpractice.com/index.php");
+	}
+
+}
